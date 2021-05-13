c291bd70e19a9f21e1b8010e9b4c73265a91b660
Immortian <andmoz2002@yandex.ru>
2021-05-13 18:06:10 +0300

    Add Task2.1

---
+++ Task2/subTask1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace subTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
