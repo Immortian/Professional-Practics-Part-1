4cb2ff3aba25525bc00177475fb9b30cfdb7cf42
Immortian <andmoz2002@yandex.ru>
2021-05-14 12:06:23 +0300

    Add subTask2

---
+++ Task2/subTask2/Classes/Collections.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace subTask2.Classes
+{
+    class CollectionType1 : HybridDictionary
+    {
+        private static HybridDictionary dictionary = new HybridDictionary();
+        public CollectionType1(int[] num, double[] sum)
+        {
+            int Length = 0;
+            if (num.Length == sum.Length)
+                Length = num.Length;
+            else if (num.Length < sum.Length)
+                Length = num.Length;
+            else
+                Length = sum.Length;
+
+            for (int i = 0; i < Length; i++)
+            {
+                dictionary.Add(num[i], sum[i]);
+            }
+        }
+        public HybridDictionary take()
+        {
+            return (HybridDictionary)dictionary;
+        }
+    }
+    class CollectionType2 : SortedList
+    {
+        private static SortedList dictionary = new SortedList();
+        public CollectionType2(int[] num, double[] sum)
+        {
+            int Length = 0;
+            if (num.Length == sum.Length)
+                Length = num.Length;
+            else if (num.Length < sum.Length)
+                Length = num.Length;
+            else
+                Length = sum.Length;
+
+            for (int i = 0; i < Length; i++)
+            {
+                dictionary.Add(num[i], sum[i]);
+            }
+        }
+        public SortedList take()
+        {
+            return (SortedList)dictionary;
+        }
+    }
+    class CollectionType3 : OrderedDictionary
+    {
+        private static OrderedDictionary dictionary = new OrderedDictionary();
+        public CollectionType3(int[] num, double[] sum)
+        {
+            int Length = 0;
+            if (num.Length == sum.Length)
+                Length = num.Length;
+            else if (num.Length < sum.Length)
+                Length = num.Length;
+            else
+                Length = sum.Length;
+
+            for (int i = 0; i < Length; i++)
+            {
+                dictionary.Add(num[i], sum[i]);
+            }
+        }
+        public OrderedDictionary take()
+        {
+            return (OrderedDictionary)dictionary;
+        }
+    }
+}
---
+++ Task2/subTask2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Specialized;
+using subTask2.Classes;
+using System.Collections;
+
+namespace subTask2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] num = new int[3] { 21313, 42578, 85732 };
+            double[] sum = new double[3] { 991313.99, 102578.98, 1285732.93 };
+            CollectionType1 cum = new CollectionType1(num, sum);
+            CollectionType2 cum2 = new CollectionType2(num, sum);
+            CollectionType3 cum3 = new CollectionType3(num, sum);
+
+           
+            foreach (var em in cum.take().Values)
+            {
+                Console.WriteLine((double)em);
+            }
+
+            foreach (var em in cum2.take().Values)
+            {
+                Console.WriteLine((double)em);
+            }
+
+            foreach (var em in cum3.take().Values)
+            {
+                Console.WriteLine((double)em);
+            }
+        }
+    }
+}
