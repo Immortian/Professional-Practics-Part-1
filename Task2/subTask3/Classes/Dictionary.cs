8a7f2b9fd03b8790e5c3107ef4c8faa4ae9bfc40
Immortian <andmoz2002@yandex.ru>
2021-05-17 15:12:38 +0300

    Add Task2 subTask3

---
+++ Task2/subTask3/Classes/Dictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace subTask3.Classes
+{
+    class Dictionary : OrderedDictionary
+    {
+        String[] myKeys;
+        String[] myValues;
+        OrderedDictionary dick = new OrderedDictionary();
+        public Dictionary(OrderedDictionary value)
+        {
+            dick = value;
+            myKeys = new String[dick.Count];
+            myValues = new String[dick.Count];
+            dick.Keys.CopyTo(myKeys, 0);
+            dick.Values.CopyTo(myValues, 0);
+        }
+        public OrderedDictionary this[int i]
+        {
+            get => (OrderedDictionary)dick;
+            set => dick = value;
+        }
+        public bool IsBiger(string Key1, string Key2)
+        {
+            
+            string element_1 = (string)dick[Key1];
+            string element_2 = (string)dick[Key2];
+
+            Console.WriteLine(element_1 + " " + element_2);
+
+            if (Convert.ToDouble(element_1) > Convert.ToDouble(element_2))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Task2/subTask3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using subTask3.Classes;
+
+namespace subTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderedDictionary orda = new OrderedDictionary();
+            orda.Add("1", "1,3123");
+            orda.Add("2", "1,1126");
+            orda.Add("3", "2,0120");
+            Dictionary dic = new Dictionary(orda);
+            Console.WriteLine( dic.IsBiger("1", "3"));
+            Console.ReadKey();
+        }
+    }
+}
