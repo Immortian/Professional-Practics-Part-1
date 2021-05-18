fa7ea6dbe5ae9dffec36e3a6dca29e57f5a15fa7
Immortian <andmoz2002@yandex.ru>
2021-05-18 11:35:55 +0300

    Add Task 3 subTask1

---
+++ Task3/subTask1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+
+namespace subTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file name");
+            string filename = Console.ReadLine();
+            Console.WriteLine("Fill this file");
+            string filefill = Console.ReadLine();
+            StreamWriter sw = new StreamWriter(@filename);
+            sw.WriteLine(filefill);
+            sw.Close();
+            Console.WriteLine($"File {filename} conteins:");
+            StreamReader sr = new StreamReader(@filename);
+            Console.Write(sr.ReadToEnd());
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
