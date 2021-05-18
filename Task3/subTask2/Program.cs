f7e1416d6b8744d9503fa64797ab474de853bf43
Immortian <andmoz2002@yandex.ru>
2021-05-18 19:13:09 +0300

    Add Task3 subTask2, Add Task3 subTask3

---
+++ Task3/subTask2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace subTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file name");
+            string path = Console.ReadLine();
+            using (FileStream fs = File.OpenRead(@path))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }
+
+                Console.WriteLine("zip?");
+                Console.ReadKey();
+
+                FileStream destination = File.Create(@"archive.zip");
+
+                GZipStream compressor = new GZipStream(destination, CompressionMode.Compress);
+
+                int theByte = fs.ReadByte();
+                while (theByte != -1)
+                {
+                    compressor.WriteByte((byte)theByte);
+                    theByte = fs.ReadByte();
+                }
+
+                compressor.Close();
+            }
+            Console.ReadKey();
+        }
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+    }
+}
---
+++ Task3/subTask3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace subTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IsolatedStorageFile userStorage = IsolatedStorageFile.GetUserStoreForAssembly();
+
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream(@"file.set", FileMode.Create, userStorage);
+
+            StreamWriter userWriter = new StreamWriter(userStream);
+            Console.WriteLine("Write something");
+            userWriter.WriteLine(Console.ReadLine());
+            userWriter.Close();
+
+            string[] files = userStorage.GetFileNames("file.set");
+
+            if (files.Length == 0)
+            {
+                Console.WriteLine("No data saved for this user");
+            }
+            else
+            {
+                // Прочитать данные из потока.
+                userStream = new IsolatedStorageFileStream(@"file.set", FileMode.Open, userStorage);
+
+                StreamReader userReader = new StreamReader(userStream);
+                string contents = userReader.ReadToEnd();
+                Console.WriteLine(contents);
+            }
+
+            // Задержка.
+            Console.ReadKey();
+        }
+    }
+}
